df0ac70ef0847d30e869b5159a1a74e8e45c4c89
santosh0208 <santoshalamuru0@msitprogram.net>
2025-06-18 17:59:10 +0530

    changes

--- src/routes/paymentSdkRoutes.ts
+++ src/routes/paymentSdkRoutes.ts
@@ -3,9 +3,14 @@ import { Cashfree, CFEnvironment } from 'cashfree-pg';
 
 const router = Router();
 
-const clientId = process.env.CASHFREE_CLIENT_ID || '';
-const clientSecret = process.env.CASHFREE_CLIENT_SECRET || '';
-const env = process.env.CASHFREE_ENV === 'production' ? CFEnvironment.PRODUCTION : CFEnvironment.SANDBOX;
+//here we should keep production when we are ready to go live
+// const clientId = process.env.CASHFREE_CLIENT_ID || '';
+// const clientSecret = process.env.CASHFREE_CLIENT_SECRET || '';
+// const env = CFEnvironment.PRODUCTION 
+
+const clientId = process.env.CASHFREE_CLIENT_ID_sandbox || '';
+const clientSecret = process.env.CASHFREE_CLIENT_SECRET_sandbox || '';
+const env = CFEnvironment.SANDBOX;
 const returnUrl = process.env.CASHFREE_RETURN_URL || 'https://welfarecanteen.in/payment-status';
 
 // Initialize Cashfree SDK
