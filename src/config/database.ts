6be1b87dc163a8dd944cf3b71af599dfd4a52d39
pavansannalaXTS <pavansannala@xtsservices.com>
2025-07-18 09:37:37 +0530

    new

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,6 +7,12 @@ dotenv.config(); // Load environment variables from .env file
 const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
   dialect: 'postgres',
   logging: false, // Enable logging for debugging
+  pool: {
+    max: 10,         // Maximum number of connections in pool
+    min: 0,          // Minimum number of connections in pool
+    acquire: 30000,  // Max time (ms) that pool will try to get connection before throwing error
+    idle: 10000      // Max time (ms) that a connection can be idle before being released
+  }
 });
 
 // Import models
