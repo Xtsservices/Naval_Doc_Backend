0cdc82e989d714ef4d1bfcf768f8caef91234f1e
pavansannalaXTS <pavan.s@xtsservices.com>
2025-08-29 10:17:08 +0530

    new

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getAllCanteens,  } from '../controllers/userController';
+import { getAllCanteens, getAllUsers,  } from '../controllers/userController';
 import authenticateToken from '../middlewares/authMiddleware'; // Import the authentication middleware
 
 const router = Router();
 
 router.get('/getAllCanteens',authenticateToken, getAllCanteens);
+router.get('/getAllUsers',authenticateToken, getAllUsers);
 
 
 export default router;
